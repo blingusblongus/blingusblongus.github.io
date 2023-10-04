bbec258087d71915d0ed02d0aee02e6670ad8f80
Nick Allen <rutherford.t.b@gmail.com>
2023-10-04 16:55:28 -0500

    add and test themed variable text-color-muted and text-color-base

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
             fontFamily: {
-                'inclusive-sans': ['"Inclusive Sans"', 'sans-serif'],
+                "inclusive-sans": ['"Inclusive Sans"', "sans-serif"],
             },
             screens: {
-                'xs': '480px',
+                xs: "480px",
+            },
+            textColor: {
+                skin: {
+                    base: "var(--color-text-base)",
+                    muted: "var(--color-text-muted)",
+                },
             },
         },
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-    ],
-}
+    },
+    plugins: [require("@tailwindcss/typography")],
+};
