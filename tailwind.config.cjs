6b742e31883fc3744301b8dcea5764aee706f173
Nick Allen <rutherford.t.b@gmail.com>
2023-10-10 13:52:15 -0500

    add primary bg colors, test on skills

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -31,6 +31,7 @@ module.exports = {
                     base: withOpacity("--color-bg-base", "hsla"),
                     muted: withOpacity("--color-bg-muted", "hsla"),
                     accent: withOpacity("--color-bg-accent", "hsla"),
+                    primary: withOpacity("--color-bg-primary", "hsla"),
                 },
             },
 
@@ -38,6 +39,7 @@ module.exports = {
                 skin: {
                     base: withOpacity("--color-bg-base", "hsla"),
                     muted: withOpacity("--color-bg-muted", "hsla"),
+                    primary: withOpacity("--color-bg-primary", "hsla"),
                 },
             },
         },
